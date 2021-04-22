8b84d8ebebdb1caac90d97b85fe2efab4b9caef9
제주집게벌레 <@DESKTOP-DA90KRE>
2021-04-22 21:38:23 +0900

    notice2.jsp

--- src/kosta/mvc/controller/Controller.java
+++ src/kosta/mvc/controller/Controller.java
@@ -7,5 +7,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface Controller {
-	ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	
+	/**
+	 * 검색
+	 * @throws Exception 
+	 * */
+	ModelAndView select(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, Exception;
+	
+	/**
+	 * 등록
+	 * */
+	ModelAndView insert(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	
+	/**
+	 * 수정
+	 * */
+	ModelAndView update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+	
+	/**
+	 * 삭제
+	 * */
+	ModelAndView delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+
 }
--- src/kosta/mvc/controller/DispatcherServlet.java
+++ src/kosta/mvc/controller/DispatcherServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Map;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,12 +35,12 @@ public class DispatcherServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//System.out.println("frontController 요청 완료!!");
 		
-		String key = request.getParameter("key"); // user 또는 elec
+		String key = request.getParameter("key");
 		String methodName = request.getParameter("methodName");
 		
 		try {
 			if(key==null || key.equals("")) {
-				key = "elec";
+				key = "notice";
 			}
 			
 			if(methodName==null || methodName.equals("")) {
